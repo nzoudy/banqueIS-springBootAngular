0e245ee542ae20c8af3ed770be9de938ca7ae3c8
nzouda urbain <nzoudy@hotmail.fr>
2016-04-15 18:11:58 +0200

    first commit

---
+++ src/main/java/com/zoudys/BanqueIsSpringBootAngularApplication.java
@@ -0,0 +1,12 @@
+package com.zoudys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanqueIsSpringBootAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanqueIsSpringBootAngularApplication.class, args);
+	}
+}
